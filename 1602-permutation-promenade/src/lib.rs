040b0f3b3dd2bd4061bdadfdb2ab19dfe74034d4
Darren Coxall <darren@darrencoxall.com>
2017-12-16 16:24:08 +0000

    Day 16 - Permutation Promenade - Part 2

---
+++ 1602-permutation-promenade/src/lib.rs
@@ -0,0 +1,123 @@
+use std::collections::HashSet;
+
+pub fn dance(cycles: usize, moves: &[&str], dancers: &mut [char]) -> () {
+    let mut iterations: usize = 0;
+    let mut resulting_formations: HashSet<String> = HashSet::new();
+    let mut formation: String = dancers.iter().collect();
+    resulting_formations.insert(formation);
+
+    while iterations < cycles {
+        for m in moves {
+            let m = Move::from(*m);
+            m.execute(dancers);
+        }
+        iterations += 1;
+        formation = dancers.iter().collect();
+        if resulting_formations.contains(&formation) {
+            iterations = cycles - (cycles % iterations);
+        } else {
+            resulting_formations.insert(formation);
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+enum Move {
+    Spin(usize),
+    Exchange(usize, usize),
+    Partner(char, char),
+}
+
+impl Move {
+    fn execute(&self, dancers: &mut [char]) -> () {
+        use Move::*;
+        match self {
+            &Spin(n) => {
+                let length = dancers.len();
+                for a in 0..n {
+                    for b in 0..length-n {
+                        let n = (length - (n + b)) + a;
+                        dancers.swap(n, n - 1);
+                    }
+                }
+            },
+            &Exchange(a, b) => dancers.swap(a, b),
+            &Partner(ref a, ref b) => {
+                let a = dancers.iter().position(|c| c == a).unwrap();
+                let b = dancers.iter().position(|c| c == b).unwrap();
+                dancers.swap(a, b);
+            },
+        }
+    }
+}
+
+impl <'a> From<&'a str> for Move {
+    fn from(val: &'a str) -> Move {
+        let mut chars = val.chars();
+        let id = chars.next().unwrap();
+        let chars: String = chars.collect();
+        let chars: Vec<_> = chars.split('/').collect();
+        match id {
+            's' => Move::Spin(chars[0].parse().unwrap()),
+            'x' => Move::Exchange(chars[0].parse().unwrap(), chars[1].parse().unwrap()),
+            'p' => Move::Partner(chars[0].parse().unwrap(), chars[1].parse().unwrap()),
+            _   => panic!("This makes no sense"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parsing_moves() {
+        assert_eq!(Move::from("s5"),    Move::Spin(5));
+        assert_eq!(Move::from("s2"),    Move::Spin(2));
+        assert_eq!(Move::from("x3/15"), Move::Exchange(3, 15));
+        assert_eq!(Move::from("x9/10"), Move::Exchange(9, 10));
+        assert_eq!(Move::from("pe/b"),  Move::Partner('e', 'b'));
+        assert_eq!(Move::from("pa/p"),  Move::Partner('a', 'p'));
+    }
+
+    #[test]
+    fn executing_spin() {
+        let mut dancers = vec!['a', 'b', 'c', 'd', 'e'];
+        let movement = Move::Spin(2);
+        movement.execute(&mut dancers);
+        assert_eq!(dancers, vec!['d', 'e', 'a', 'b', 'c']);
+        let movement = Move::Spin(4);
+        movement.execute(&mut dancers);
+        assert_eq!(dancers, vec!['e', 'a', 'b', 'c', 'd']);
+    }
+
+    #[test]
+    fn executing_exchange() {
+        let mut dancers = vec!['a', 'b', 'c', 'd', 'e'];
+        let movement = Move::Exchange(2, 4);
+        movement.execute(&mut dancers);
+        assert_eq!(dancers, vec!['a', 'b', 'e', 'd', 'c']);
+        let movement = Move::Exchange(0, 1);
+        movement.execute(&mut dancers);
+        assert_eq!(dancers, vec!['b', 'a', 'e', 'd', 'c']);
+    }
+
+    #[test]
+    fn executing_partner() {
+        let mut dancers = vec!['a', 'b', 'c', 'd', 'e'];
+        let movement = Move::Partner('d', 'a');
+        movement.execute(&mut dancers);
+        assert_eq!(dancers, vec!['d', 'b', 'c', 'a', 'e']);
+        let movement = Move::Partner('b', 'e');
+        movement.execute(&mut dancers);
+        assert_eq!(dancers, vec!['d', 'e', 'c', 'a', 'b']);
+    }
+
+    #[test]
+    fn executing_dance() {
+        let mut dancers = vec!['a', 'b', 'c', 'd', 'e'];
+        let moves = vec!["s1", "x3/4", "pe/b"];
+        dance(2, &moves, &mut dancers);
+        assert_eq!(dancers, vec!['c', 'e', 'a', 'd', 'b']);
+    }
+}
---
+++ 1602-permutation-promenade/src/main.rs
@@ -0,0 +1,19 @@
+extern crate permutationpromenade;
+
+use permutationpromenade::dance;
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let stdin = io::stdin();
+    let handle = stdin.lock();
+    let mut dancers = vec![
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+        'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    ];
+    let lines: Vec<_> = handle.lines().filter_map(|line| line.ok()).collect();
+    let moves: Vec<_> = lines.iter().flat_map(|line| line.split(',')).collect();
+    dance(1_000_000_000, &moves, &mut dancers);
+    let result: String = dancers.iter().collect();
+    println!("{}", result);
+}
